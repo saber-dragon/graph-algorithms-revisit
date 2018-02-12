51054337c0226048e7ab8cd56da1ec03a5014ae8
Long Gong <long.github@gmail.com>
2018-02-11 22:14:31 -0500

    update

--- graph_reader_test.cpp
+++ graph_reader_test.cpp
@@ -2,24 +2,30 @@
 // Created by saber on 2/9/2018.
 //
 #include <iostream>
-#include <fstream>
 #include <lemon/list_graph.h>
 #include <lemon/lgf_reader.h>
-
 
 using namespace lemon;
 
 
 int main()
 {
-    typedef ListGraph Graph;
+    typedef ListDigraph Graph;
     typedef Graph::Node Node;
     typedef Graph::Arc Arc;
     using NodeIt = Graph::NodeIt;
     using ArcIt = Graph::ArcIt;
     Graph g;
-    try    std::string filename("example.dim");
-    GraphReader<Graph> reader(g, filename);
+    std::string filename("example.lfg");
+    try{
+        DigraphReader<Graph> reader(g, filename);
+        reader.run();
+    }
+    catch (Exception& error) { // check if there was any error
+        std::cerr << "Error: " << error.what() << std::endl;
+        return -1;
+    }
+
 
     std::cout << "Nodes:";
     for (NodeIt i(g); i!=INVALID; ++i)
